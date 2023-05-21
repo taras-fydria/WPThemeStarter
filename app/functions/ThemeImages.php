84c772a4e2e0246d4de648d2485f5ab4150b602d
taras <taras.fydria@gmail.com>
2023-05-21 15:24:13 +0300

    add svg support

--- app/functions/ThemeImages.php
+++ app/functions/ThemeImages.php
@@ -50,9 +50,13 @@ class ThemeImages extends Singleton
     {
         parent::__construct();
         $this->add_custom_image_sizes();
+        // upload svg
         add_filter('upload_mimes', [$this, 'support_for_upload_svg_files']);
-        add_filter('wp_check_filetype_and_ext', [$this, 'svgs_upload_check'], 10, 4);
-        add_filter( 'big_image_size_threshold', '__return_false' );
+
+
+        add_filter('wp_check_filetype_and_ext', [$this, 'fix_svg_mime_type'], 10, 5);
+
+
     }
 
     public function support_for_upload_svg_files(array $mimes = []): array
@@ -65,27 +69,29 @@ class ThemeImages extends Singleton
     }
 
     /**
-     * Check Mime Types
+     * Fix MIME type for SVG files.\
      */
-    public function svgs_upload_check(array $checked, string $file, string $filename, array $mimes)
+    public function fix_svg_mime_type($data, $file, $filename, $mimes, $real_mime = '')
     {
+        // WP 5.1 +
+        if (version_compare($GLOBALS['wp_version'], '5.1.0', '>='))
+            $dosvg = in_array($real_mime, ['image/svg', 'image/svg+xml']);
+        else
+            $dosvg = ('.svg' === strtolower(substr($filename, -4)));
 
-        if (!$checked['type']) {
+        if ($dosvg) {
 
-            $check_filetype = wp_check_filetype($filename, $mimes);
-            $ext = $check_filetype['ext'];
-            $type = $check_filetype['type'];
-            $proper_filename = $filename;
+            if (current_user_can('manage_options')) {
 
-            if ($type && 0 === strpos($type, 'image/') && $ext !== 'svg') {
-                $ext = $type = false;
+                $data['ext'] = 'svg';
+                $data['type'] = 'image/svg+xml';
+            } else {
+                $data['ext'] = $type_and_ext['type'] = false;
             }
 
-            $checked = compact('ext', 'type', 'proper_filename');
         }
 
-        return $checked;
-
+        return $data;
     }
 
 
